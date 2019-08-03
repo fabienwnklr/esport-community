ece8aa89a9fa93f59ec8f7c70d71f669436295bd
fabien <fabien.winkler@outlook.fr>
2019-08-04 00:11:08 +0200

    test

--- pages/contact-us.php
+++ pages/contact-us.php
@@ -37,7 +37,7 @@
                 <div class="card-header bg-success text-white text-uppercase"><i class="fa fa-home"></i> Adresse</div>
                 <div class="card-body">
                     <p>3 rue des Champs Elysées</p>
-                    <p>75008 PARIS</p>
+                    <p>64200 Biarritz</p>
                     <p>France</p>
                     <p>Email : email@example.com</p>
                     <p>Tel. 06 12 56 11 51 84</p>
