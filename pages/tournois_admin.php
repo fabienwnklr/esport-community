dc42dbb0d4f3933276ff25a802798ebee5dfbaef
fabien <fabien.winkler@outlook.fr>
2019-08-12 20:00:45 +0200

    header + input css

--- pages/tournois_admin.php
+++ pages/tournois_admin.php
@@ -7,7 +7,7 @@ $tournaments = displayTournament('name ASC');
 
     <div class="container-tournament wrapper">
         <div class="global-display">
-            <a href="http://localhost/php/esport-community/pages/creer-tournoi.php">
+            <a href="creer-tournoi">
                 <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
                     <circle style="fill:#43B05C;" cx="25" cy="25" r="25" />
                     <line style="fill:none;stroke:#FFFFFF;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" x1="25" y1="13" x2="25" y2="38" />
